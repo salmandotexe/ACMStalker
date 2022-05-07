a70cdb0d21d900ffc456d3f41393978e22f1b252
Salman Sahel <salman.exe@gmail.com>
2022-05-07 15:00:34 +0600

    Working backend

--- server/controllers/cfRatings.js
+++ server/controllers/cfRatings.js
@@ -1,4 +1,20 @@
+import UserCFData from "../models/UserCFdata.js";
+import axios from 'axios';
+import mongoose from "mongoose";
 
-export const getCFRatings = (req, res) => {
-    res.send("CF Ratings Page");
+export const getUserRating = async (req, res) => {
+    try {
+        const {uid} = req.params;
+        if(! mongoose.Types.ObjectId.isValid(uid)) return res.status(404).send("Invalid id");
+        
+        const url = 'https://codeforces.com/api/user.rating?handle='+uid;
+
+        axios.get(url).then((cfresponse) => {
+            const results = cfresponse.data.status =='OK'? cfresponse.data.result: [];
+            //console.log(results);
+            res.status(201).json({ratings: results});
+        });
+    } catch (error) {
+        res.status(409).json(error);
+    }
 }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import {createUser, getUsers} from '../controllers/userController.js';
+import {getUserRating} from '../controllers/cfRatings.js';
+
 const router = express.Router();
 
 router.get('/', getUsers);
 router.post('/', createUser);
-
+router.get('/ratings/:uid', getUserRating);  // http://localhost:5000/users/ratings
 export default router;
